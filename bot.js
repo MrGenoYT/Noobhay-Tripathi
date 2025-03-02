dee2978fdc08159097028f75cdfbf544cb6568a7
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-03-02 17:35:20 +0530

    bot.js

--- bot.js
+++ bot.js
@@ -363,6 +363,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
     GatewayIntentBits.GuildMembers,
   ],
   partials: [Partials.Channel],
